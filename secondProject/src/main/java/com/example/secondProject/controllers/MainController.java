34cc402908361fc3c729f8246e8dda3e91318532
gtol94 <gaukhartast@gmail.com>
2021-01-23 20:16:59 +0600

    My first Commit.

---
+++ secondProject/src/main/java/com/example/secondProject/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.example.secondProject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World")String name,
+            Map<String, Object> model
+                           ){
+        model.put("name", name);
+        return "greeting";
+    }
+}
